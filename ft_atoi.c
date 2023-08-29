dfbd95d6841db3be790971558e88f0ffbee5e15e
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-29 15:02:17 -0300

    Finished :)

---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 12:45:28 by eddos-sa          #+#    #+#             */
+/*   Updated: 2023/08/10 12:44:55 by eddos-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	unsigned int	i;
+	int				sign;
+	int				result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] && ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13)))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
